d41bde0fb593c68ef62117ab98b62b5242bed6a9
wengjiaojiao <wengjiaojiao@outlook.com>
2015-11-17 20:01:23 +0800

    js:add the highLight function

--- http-server/public/index.js
+++ http-server/public/index.js
@@ -4,20 +4,33 @@ function dataPrinter(data) {
   })
 }
 
+function highLight(idVal,keyword, color) {
+    var hlValue = new RegExp("(" + keyword + ")(?=[^<>]*<)","gi");
+    $ID(idVal).innerHTML = $ID(idVal).innerHTML.replace(hlValue, "<font color=" + color + ">$1</font>");
+}
+
+function $ID(idVal) {
+    return document.getElementById(idVal);
+}
+
 $.get("./bookmarks.json", function(data) {
     dataPrinter(data);
     $(".search-frame").on('keyup',function() {
         var searchText = $('.search-frame').val();
         var newData = [];
 
-        data.forEach(function(val) {
-            if(val.title.indexOf(searchText) !== -1) {
+        _.forEach(data, function(val) {
+            var title = val.title.toLowerCase();
+
+            if(title.indexOf(searchText) !== -1) {
                 $('.prompts').empty();
-                newData.push({title:val.title});
+                newData.push({title: val.title,
+                              created: val.created});
             }else{
                 $('.prompts').empty();
             }
         });
         dataPrinter(newData);
+        highLight("prompts", searchText, "red");
     });
 });
