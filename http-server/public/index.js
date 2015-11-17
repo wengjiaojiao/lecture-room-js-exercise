d0a24d2a2610fea7e37e447873bfdba197575355
wengjiaojiao <wengjiaojiao@outlook.com>
2015-11-17 18:47:25 +0800

    js:use ajax to require the json data

--- http-server/public/index.js
+++ http-server/public/index.js
@@ -1,28 +1,23 @@
-var data = [
-    {firstName: "bill",lastName: "Gates"},
-    {firstName: "george",lastName: "Bush"},
-    {firstName: "thomas",lastName: "Carter"}
-];
-
 function dataPrinter(data) {
   data.forEach(function(val) {
-      $("<li>" + val.firstName + "</li>").appendTo(".prompts");
+      $("<li>" + val.title + "</li>").appendTo(".prompts");
   })
 }
 
-dataPrinter(data);
-$(".search-frame").on('keyup',function() {
-    var searchText = $('.search-frame').val();
-    var newData = [];
+$.get("./bookmarks.json", function(data) {
+    dataPrinter(data);
+    $(".search-frame").on('keyup',function() {
+        var searchText = $('.search-frame').val();
+        var newData = [];
 
-    data.forEach(function(val) {
-        if(val.firstName.indexOf(searchText) !== -1) {
-            $('.prompts').empty();
-            newData.push({firstName:val.firstName});
-        }else{
-            $('.prompts').empty();
-        }
+        data.forEach(function(val) {
+            if(val.title.indexOf(searchText) !== -1) {
+                $('.prompts').empty();
+                newData.push({title:val.title});
+            }else{
+                $('.prompts').empty();
+            }
+        });
+        dataPrinter(newData);
     });
-    dataPrinter(newData);
-
 });
